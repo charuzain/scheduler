a20e49b16bf8f051804b15f8c0c28708d97a903b
charuzain <charuabhizain@gmail.com>
2021-12-09 14:48:44 -0500

    classnames library imported & Button component Refactored

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,17 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
-   if(props.confirm){
-      buttonClass = buttonClass +" button--confirm"
-   }
-   if (props.danger) {
-      buttonClass = buttonClass + " button--danger"
-   }
+   // let buttonClass = "button";
+  const buttonClass= classNames('button', { 'button--confirm': props.confirm, 'button--danger': props.danger})
+   // if(props.confirm){
+   //    buttonClass = buttonClass +" button--confirm"
+   // }
+   // if (props.danger) {
+   //    buttonClass = buttonClass + " button--danger"
+   // }
    return <>
    <button onClick={props.onClick} className={buttonClass} disabled={props.disabled}>{props.children}</button>
    </>;
