b758fab681644445e3854faf079418429629bca5
charuzain <charuabhizain@gmail.com>
2021-12-09 13:49:50 -0500

    Button component created

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,14 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+   if(props.confirm){
+      buttonClass = buttonClass +" button--confirm"
+   }
+   if (props.danger) {
+      buttonClass = buttonClass + " button--danger"
+   }
+   return <>
+   <button onClick={props.onClick} className={buttonClass} disabled={props.disabled}>{props.children}</button>
+   </>;
 }
