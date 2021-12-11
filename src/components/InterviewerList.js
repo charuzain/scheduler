8264d34ec8a7527b0be71935ee790a42d6aa19dc
charuzain <charuabhizain@gmail.com>
2021-12-10 23:28:17 -0500

    InterviewList Component implemented

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './InterviewerList.scss';
+import './InterviewerListItem';
+import InterviewerListItem from './InterviewerListItem';
+
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers;
+  const setInterviewer = props.setInterviewer;
+  const selectedInterviewer = props.interviewer;
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+      {interviewers.map((interviewer)=> <InterviewerListItem key={interviewer.id}
+                                                              id={interviewer.id} 
+                                                              name={interviewer.name}
+                                                              avatar={interviewer.avatar}
+                                                              selected={selectedInterviewer === interviewer.id} 
+                                                              setInterviewer={setInterviewer}/>)}
+      </ul>
+    </section>
+  )
+}
