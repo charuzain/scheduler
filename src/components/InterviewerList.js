aba6d41c26463fcd50bbfc21c051baa7663ee007
charuzain <charuabhizain@gmail.com>
2021-12-11 15:42:55 -0500

    Refactored InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import './InterviewerList.scss';
-import './InterviewerListItem';
 import InterviewerListItem from './InterviewerListItem';
 
 
 export default function InterviewerList(props) {
   const interviewers = props.interviewers;
-  const setInterviewer = props.setInterviewer;
-  const selectedInterviewer = props.interviewer;
+  const onChange = props.onChange;
+  const value = props.value;
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-      {interviewers.map((interviewer)=> <InterviewerListItem key={interviewer.id}
-                                                        name={interviewer.name}
-                                                        avatar={interviewer.avatar}
-                                                        selected={selectedInterviewer === interviewer.id} 
-                                                        setInterviewer={(event) => setInterviewer(interviewer.id)}/>)}
+      {interviewers.map((interviewer)=> (
+        <InterviewerListItem key={interviewer.id}
+                             name={interviewer.name}
+                            avatar={interviewer.avatar}
+                           selected={interviewer.id===value} 
+                          setInterviewer={() => onChange(interviewer.id)}/>))}
       </ul>
     </section>
   )
