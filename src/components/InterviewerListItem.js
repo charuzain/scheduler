08a506cea4c89a0411067ee52c1361891e0e3e0e
charuzain <charuabhizain@gmail.com>
2021-12-11 00:05:07 -0500

    Refactored InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,20 +3,16 @@ import "./InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const { id, name, avatar, selected, setInterviewer} = props;
-  // const onClickHandler = function(){setInterviewer(id)}
+  const {name, avatar, selected, setInterviewer} = props;
   const interviewerClass = classNames('interviewers__item', {'interviewers__item--selected':selected})
   return (
-    <li className={interviewerClass} onClick={() => setInterviewer(id)}>
+    <li className={interviewerClass} onClick={setInterviewer}>
       <img
         className="interviewers__item-image"
         src={avatar}
         alt={name}
       />
-    
       {selected && name}
-   
     </li>
-  
   )
 }
