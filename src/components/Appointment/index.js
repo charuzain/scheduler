35850a823d042a22149bea29fcf8294dbba4dea1
charuzain <charuabhizain@gmail.com>
2021-12-12 20:22:13 -0500

    Show & Empty component rendered

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import './style.scss'
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+import './style.scss';
 
 export default function Appointment(props) {
 
   return (
-      <article className="appointment">
+    
+    <article className="appointment">
+    <Header time={props.time}/>
+      {props.interview ? <Show {...props.interview}/> : <Empty />}
+      {/* <article className="appointment">
         {
           props.time ? `Appointment at ${props.time}` : "No appointments"
         }
-      </article>
+      </article> */}
+    </article> 
   )
 }
+// student = { props.interview.student } interviewer = { props.interview.interviewer }
 
