377e206eb8599a81769449a06b57189cf6564d66
charuzain <charuabhizain@gmail.com>
2021-12-12 20:20:39 -0500

    Rendered Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,9 @@
 import React,{useState} from "react";
 
 import "components/Application.scss";
+import "components/Appointment"
 import DayList from "./DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -20,6 +22,46 @@ const days = [
     spots: 0,
   },
 ]
+
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 
 export default function Application(props) {
@@ -43,10 +85,10 @@ export default function Application(props) {
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment=><Appointment key={appointment.id} {...appointment} />)}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
