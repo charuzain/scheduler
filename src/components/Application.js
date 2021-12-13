bcf46a5de2b0140e51efce932a27c4e7b6b625e6
charuzain <charuabhizain@gmail.com>
2021-12-13 00:27:42 -0500

    Combined states in Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,49 +7,58 @@ import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//   }
+// ];
 
 
 export default function Application(props) {
-  const [days,setDays] = useState([]);
-  const [day, setDay] = useState([]);
+  // const [days,setDays] = useState([]);
+  // const [day, setDay] = useState([]);
+
+  const [state, setState] = useState({
+    day : "Monday",
+    days : [],
+    appointments:{}
+  })
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
 
   useEffect(() => {
     axios.get('/api/days').then((response)=>{setDays(response.data)})
@@ -65,7 +74,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList days={days} value={day} onChange={setDay}/>
+        <DayList days={state.days} value={state.day} onChange={setDay}/>
 
         </nav>
         <img
