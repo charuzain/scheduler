476d654c492354c72067687233bf163fc1a9552e
charuzain <charuabhizain@gmail.com>
2021-12-13 19:36:40 -0500

    getInterview selector added

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import "components/Application.scss";
 import "components/Appointment"
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -52,7 +52,13 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map(appointment=><Appointment key={appointment.id} {...appointment} />)}
+        {dailyAppointments.map((appointment)=>
+        { const interview = getInterview(state,appointment.interview);
+        return(
+        <Appointment key={appointment.id}
+         id={appointment.id}
+         time={appointment.time}
+         interview={interview} />)})}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
