e93e977c20bae1dede017478f35b20c703767b31
charuzain <charuabhizain@gmail.com>
2021-12-14 11:46:02 -0500

    Rendered SHOW , EMPTY , CREATE MODE using useVisualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import useVisualMode from 'hooks/useVisualMode';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 import './style.scss';
+import Form from './Form';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  )
 
   return (
     
     <article className="appointment">
     <Header time={props.time}/>
-      {props.interview ? <Show {...props.interview}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={()=>transition(CREATE)}/>}
+      {mode === CREATE && <Form interviewers={[]}
+                                  onCancel={()=>{back(EMPTY)}}/>}
+      {mode === SHOW && <Show student={props.interview.student}
+        interviewer={props.interview.interviewer}/>}
+      {/* {props.interview ? <Show {...props.interview}/> : <Empty />} */}
       {/* <article className="appointment">
         {
           props.time ? `Appointment at ${props.time}` : "No appointments"
