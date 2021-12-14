abfb22c0644ebfeca1aab83b0e0b968ec4fa2f27
charuzain <charuabhizain@gmail.com>
2021-12-14 10:31:22 -0500

    useVisualMode Hook implemented

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+
+const useVisualMode =(initialMode)=>{
+
+ const [mode,setMode] = useState(initialMode);
+ const [history, setHistory] = useState([initialMode])
+  
+  function transition(newMode,replace=false){
+    if(replace){
+      setMode(newMode)
+    }
+    else{
+   setMode(newMode)
+   setHistory([...history,newMode])}
+                }
+
+ function back(){
+   if(history.length > 1){
+   history.pop()
+    setMode(history[history.length-1])}
+
+ }
+
+
+
+
+ return {mode, transition,back }
+
+
+}
+  
+
+
+export default useVisualMode;
