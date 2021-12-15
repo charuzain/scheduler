0a12b1e05521a830543d7025ec6ef5557bc7149f
charuzain <charuabhizain@gmail.com>
2021-12-14 21:54:51 -0500

    bookInterview function implemented

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import "components/Application.scss";
 import "components/Appointment"
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -18,6 +18,29 @@ export default function Application(props) {
   const setDay = day => setState({ ...state, day });
   // const setDays = days => setState(prev => ({ ...prev, days }));
   const dailyAppointments = getAppointmentsForDay(state, state.day)
+  // const dailyInterviewers=  getInterviewersForDay(state, state.day)
+  
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    // setState({ ...state, appointments })
+
+    return axios.put(`/api/appointments/${id}`, appointment)
+      .then(() => {
+        setState({ ...state, appointments })
+      })
+
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
 
   useEffect(() => {
     Promise.all([
@@ -25,13 +48,12 @@ export default function Application(props) {
       axios.get("http://localhost:8001/api/appointments"),
       axios.get("http://localhost:8001/api/interviewers")
     ]).then((result)=>{
-      console.log(result[0].data)
-      console.log(result[1].data)
-      console.log(result[2].data)
       
       setState(prev => ({ ...prev, days: result[0].data, appointments: result[1].data, interviewers: result[2].data}))
     })
   }, [])
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -54,12 +76,17 @@ export default function Application(props) {
       <section className="schedule">
         {dailyAppointments.map((appointment)=>
         { const interview = getInterview(state,appointment.interview);
+          const interviewers = getInterviewersForDay(state, state.day)
         return(
         <Appointment key={appointment.id}
          id={appointment.id}
          time={appointment.time}
-         interview={interview} />)})}
-        <Appointment key="last" time="5pm" />
+         interview={interview}
+        interviewers={interviewers}
+        bookInterview={bookInterview}/>)})}
+        <Appointment key="last" 
+                     time="5pm"
+                    bookInterview={bookInterview} />
       </section>
     </main>
   );
