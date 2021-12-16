ba2226d32aa8d8d8467f9b6260dc3f625cbd7c8f
charuzain <charuabhizain@gmail.com>
2021-12-16 18:40:38 -0500

    Implemented Form Validation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,15 +6,25 @@ export default function Form(props) {
   
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  
+  const [error, setError] = useState("");
   const reset = function(){
     setStudent("")
     setInterviewer("")
+    setError("")
   }
 
   const cancel = function(){
     reset()
     props.onCancel()
+  }
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("")
+    props.onSave(student, interviewer);
   }
 
   return (
@@ -25,10 +35,12 @@ export default function Form(props) {
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder="Enter your name"
-            value ={student}
+            placeholder="Enter Student Name"
+            value={student}
             onChange={(e)=>setStudent(e.target.value)}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -39,7 +51,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={()=>props.onSave(student,interviewer)} >Save</Button>
+          <Button confirm onClick={validate} >Save</Button>
         </section>
       </section>
     </main>
