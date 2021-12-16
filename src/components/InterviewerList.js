dc7deeda3a71ca5a00d9f21b29a29a360adde14d
charuzain <charuabhizain@gmail.com>
2021-12-16 18:47:21 -0500

    Interviewers props validated

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,19 +5,19 @@ import InterviewerListItem from './InterviewerListItem';
 
 
 export default function InterviewerList(props) {
-  const interviewers = props.interviewers;
-  const onChange = props.onChange;
-  const value = props.value;
+              const interviewers = props.interviewers;
+              const onChange = props.onChange;
+              const value = props.value;
   return (
-    <section className="interviewers">
+      <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
       {interviewers.map((interviewer)=> (
-        <InterviewerListItem key={interviewer.id}
+      <InterviewerListItem key={interviewer.id}
                              name={interviewer.name}
-                            avatar={interviewer.avatar}
-                           selected={interviewer.id===value} 
-                          setInterviewer={() => onChange(interviewer.id)}/>))}
+                             avatar={interviewer.avatar}
+                             selected={interviewer.id===value} 
+                             setInterviewer={() => onChange(interviewer.id)}/>))}
       </ul>
     </section>
   )
