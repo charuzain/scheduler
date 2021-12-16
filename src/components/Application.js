cee06e4798a3a0a22f1de204a0a594494ea4af4f
charuzain <charuabhizain@gmail.com>
2021-12-15 22:02:42 -0500

    useApplicationData hook imported & code refactored

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,72 +1,74 @@
-import React,{useState, useEffect} from "react";
-import axios from "axios";
+ import React from "react";
+// import axios from "axios";
 
 import "components/Application.scss";
 import "components/Appointment"
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
-
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day : "Monday",
-    days : [],
-    appointments:{},
-    interviewers:{}
-  })
-  const setDay = day => setState({ ...state, day });
+
+ const { state, setDay, bookInterview, cancelInterview } = useApplicationData()
+  // const [state, setState] = useState({
+  //   day : "Monday",
+  //   days : [],
+  //   appointments:{},
+  //   interviewers:{}
+  // })
+  // const setDay = day => setState({ ...state, day });
   const dailyAppointments = getAppointmentsForDay(state, state.day)
   
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.put(`/api/appointments/${id}`, appointment)
-      .then(() => {
-        setState({ ...state, appointments })
-      })
+  // function bookInterview(id, interview) {
+  //   const appointment = {
+  //     ...state.appointments[id],
+  //     interview: { ...interview }
+  //   };
+  //   const appointments = {
+  //     ...state.appointments,
+  //     [id]: appointment
+  //   };
+  //   return axios.put(`/api/appointments/${id}`, appointment)
+  //     .then(() => {
+  //       setState({ ...state, appointments })
+  //     })
 
-    .catch((err)=>{
-      console.log(err)
-    })
-  }
-
-
-  function cancelInterview(id){
-    const appointment ={
-      ...state.appointments[id] , 
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {
-        setState({ ...state, appointments })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
+  //   .catch((err)=>{
+  //     console.log(err)
+  //   })
+  // }
 
 
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
-    ]).then((result)=>{
+  // function cancelInterview(id){
+  //   const appointment ={
+  //     ...state.appointments[id] , 
+  //     interview: null
+  //   }
+  //   const appointments = {
+  //     ...state.appointments,
+  //     [id]: appointment
+  //   }
+  //   return axios.delete(`/api/appointments/${id}`)
+  //     .then(() => {
+  //       setState({ ...state, appointments })
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // }
+
+
+  // useEffect(() => {
+  //   Promise.all([
+  //     axios.get("http://localhost:8001/api/days"),
+  //     axios.get("http://localhost:8001/api/appointments"),
+  //     axios.get("http://localhost:8001/api/interviewers")
+  //   ]).then((result)=>{
       
-      setState(prev => ({ ...prev, days: result[0].data, appointments: result[1].data, interviewers: result[2].data}))
-    })
-  }, [])
+  //     setState(prev => ({ ...prev, days: result[0].data, appointments: result[1].data, interviewers: result[2].data}))
+  //   })
+  // }, [])
 
 
   return (
@@ -104,8 +106,6 @@ export default function Application(props) {
         />)})}
         <Appointment key="last" 
                      time="5pm"
-                    //  bookInterview={bookInterview}
-                    //  cancelInterview={cancelInterview}
                      />
       </section>
     </main>
