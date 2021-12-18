15058ad97dbea092a471813d6a36615d6f2b4ca2
charuzain <charuabhizain@gmail.com>
2021-12-17 20:02:53 -0500

    Removed unused code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,35 +1,34 @@
 export function getAppointmentsForDay(state, day) {
- const appointmentForDay =[];
- for(let elem of state.days){
-   if(elem.name===day){
-     for(let appointment of elem.appointments){
-       appointmentForDay.push(state.appointments[appointment])
-     }
-   }
- }
-return appointmentForDay
-}
-
-export function getInterview(state,interview){
-if(interview){
-  const interviewerId = interview.interviewer
-  const parsedInterview = {};
-  parsedInterview.student = interview.student;
-  parsedInterview.interviewer = state.interviewers[interviewerId]
-  return parsedInterview
-}
-return null;
-}
-
-
-export function getInterviewersForDay(state, day) {
-  const parsedInterviewers = [];
-  for(let elem of state.days){
-    if(elem.name === day){
-      for(let interviewer of elem.interviewers ){
-        parsedInterviewers.push(state.interviewers[interviewer])
+  const appointmentForDay = [];
+  for (let elem of state.days) {
+    if (elem.name === day) {
+      for (let appointment of elem.appointments) {
+        appointmentForDay.push(state.appointments[appointment]);
       }
     }
   }
- return parsedInterviewers
+  return appointmentForDay;
+}
+
+export function getInterview(state, interview) {
+  if (interview) {
+    const interviewerId = interview.interviewer;
+    const parsedInterview = {};
+    parsedInterview.student = interview.student;
+    parsedInterview.interviewer = state.interviewers[interviewerId]
+    return parsedInterview;
+  }
+  return null;
+}
+
+export function getInterviewersForDay(state, day) {
+  const parsedInterviewers = [];
+  for (let elem of state.days) {
+    if (elem.name === day) {
+      for (let interviewer of elem.interviewers) {
+        parsedInterviewers.push(state.interviewers[interviewer]);
+      }
+    }
+  }
+  return parsedInterviewers;
 }
